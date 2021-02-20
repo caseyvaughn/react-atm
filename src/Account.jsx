d6a2b1ede388c2ce75651eb5366a01a2375b035e
Soleil Solomon <mounlasolomon@gmail.com>
2021-02-19 21:45:14 -0500

    Update Account.jsx

--- src/Account.jsx
+++ src/Account.jsx
@@ -4,8 +4,8 @@ function Account() {
       <h2>TODO: CHANGE TO PROP</h2>
       <div className="balance">$0</div>
       <input type="text" placeholder="enter an amount" />
-      <input type="button" value="Deposit" />
-      <input type="button" value="Withdraw" />
+      <button>Deposit</button>
+      <button>Withdraw</button>
     </div>
   )
 }
