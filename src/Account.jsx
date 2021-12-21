017bcc80dafef472ba276e4711eb50966b6cb6bc
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2021-12-21 18:43:47 -0500

    able to console log updated balance but will not display on page

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,12 +1,48 @@
+import { useState } from "react"
+
 function Account(props) {
+  //add a new useState for balance & input
+  const [balance, setBalance] = useState(0);
+  const [input, setInput] = useState("");
+
+  //create an event listener to store current value of the input state
+  const handleInput = (e) => {
+    e.preventDefault();
+    setInput(e.target.valueAsNumber);
+  }
+
+  //handle deposit
+  // const handleDeposit = (e) => {
+  //   e.preventDefault();
+  //   setBalance(balance + input);
+  //   console.log(`New Balance: ${balance}`);
+  //   console.log(typeof balance);
+  // }
+
+  //simplified handleDeposit
+  const handleDeposit = () => {
+    setBalance(balance + input);
+    console.log(`New Balance After Deposit: ${balance}`);
+    console.log(typeof balance);
+  }
+
+  const handleWithdraw = () => {
+    setBalance(balance - input);
+    console.log(`New Balance After Withdraw: ${balance}`);
+  }
+
+  if (balance === 0) {
+    
+  }
+
   return (
     <div className="account">
       <h2>{props.name}</h2>
-      <div className="balance">$0</div>
-      <input type="text" placeholder="enter an amount" />
-      <button>Deposit</button>
-      <button>Withdraw</button>
-    </div>
+        <div className="balance">$0</div>
+          <input type="number" placeholder="enter an amount" onChange={handleInput}/>
+          <button onClick={handleDeposit}>Deposit</button>
+      <button disabled={balance === input} onClick={handleWithdraw}>Withdraw</button>
+        </div>
   )
 }
 
